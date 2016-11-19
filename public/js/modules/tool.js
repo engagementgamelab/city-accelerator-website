9c80ed3346dbeb9da771d314e4094dc6b9ad7387
Mariano Miguel <mariano@mariano.io>
2016-11-19 01:38:59 -0300

    Re-do a lot

--- public/js/modules/tool.js
+++ public/js/modules/tool.js
@@ -3,12 +3,8 @@
   container = "";
   main = "";
   views = "";
-  active_view = "";
-  active_step = "";
-  active_sub_step = "";
+  steps = "";
   active_theme = "";
-  prev_view = "";
-  next_view = "";
   first_view = true;
   print_array = [];
   transition_in_props = {
@@ -21,18 +17,6 @@
   };
 
 
-
-
-
-
-  function updateProgress() {
-    var amount = $(views).last().index() + 1;
-    var current = active_view.index() + 1;
-    var percentage = (current * 100 / amount) + '%';
-    //var percentage = (current * 100 / amount) / 100;
-    //var new_width = $('.js-tool-steps-list').outerWidth() * percentage;
-    $('.js-tool-progress').velocity({ width: percentage });
-  }
 
   function addToPrintable(object) {
     if($.inArray(object, print_array) == -1) {
@@ -48,6 +32,11 @@
     print_array = print_array.sort((a, b) => a - b);
   }
 
+
+
+
+
+
   function transitionIn(object) {
     object.addClass('is-active');
     object.velocity(transition_in_props);
@@ -58,133 +47,93 @@
     object.velocity(transition_out_props);
   }
 
+  function updateMainHeight() {
+    main.css('height', $('.js-tool-view.is-active').outerHeight());
+  }
+
   function updateView() {
+    var data = active_view.split('.');
+    var step = data[0];
+    var sub_step = data[1];
 
-    active_step = active_view.data('step');
-    active_sub_step = active_view.data('sub-step');
+    updateMainHeight();
 
-    var old_step = $('.js-tool-view.is-active').data('step');
-    var old_sub_step = $('.js-tool-view.is-active').data('sub-step');
-
-    if(old_step == active_step && old_sub_step == active_sub_step && first_view == false) {
-      return false;
-    }
-
-    // Update Theme
     if (body.hasClass(active_theme)) {
       body.removeClass(active_theme);
     }
-    active_theme = active_view.data('theme');
-    body.addClass(active_theme);
 
-    // Change View
-    transitionOut($('.js-tool-view.is-active'));
-    transitionIn(active_view);
+    active_theme = $('.js-tool-step').eq(step).attr('data-theme');
+    body.addClass(active_theme);
 
     updateProgress();
 
-    first_view = false;
-    //body.addClass((window.active_view).attr('data-theme'));
-    //main.css('height', main.outerHeight() + active_view.outerHeight());
-  }
-
-  function enablePrev() {
-    $('.js-tool-back').removeClass('is-disabled');
-  }
-
-  function disablePrev() {
-    $('.js-tool-back').addClass('is-disabled');
-  }
-
-  function getLastStep() {
-    var highest = $('.js-tool-view').map(function() {
-      return parseFloat($(this).data('step'));
-    }).get().sort().pop();
-
-    return highest;
-  }
-
-  function getLastSubStep(step) {
-    var highest = $('.js-tool-view[data-step="' + step + '"]').map(function() {
-      return parseFloat($(this).data('sub-step'));
-    }).get().sort().pop();
-
-    return highest;
-  }
-
-  function getPrevStep(step, sub_step) {
-    var new_sub_step = 0;
-    var new_step = 0;
-
-    console.log(step - 1);
-
-    if(sub_step - 1 >= 1) {
-      new_step = step;
-      new_sub_step = sub_step - 1;
-    } else {
-      if(step - 1 >= 1) {
-        new_step = step - 1;
-        new_sub_step = getLastSubStep(new_step);
-      } else {
-        new_step = 1;
-        new_sub_step = 1;
-      }
-    }
-    var view = $('.js-tool-view[data-step="' + new_step + '"][data-sub-step="' + new_sub_step + '"]' );
-    return view;
-  }
-
-  function getNextStep(step, sub_step) {
-    var new_sub_step = 0;
-    var new_step = 0;
-
-    if(sub_step + 1 <= getLastSubStep(step)) {
-      new_step = step;
-      new_sub_step = sub_step + 1;
-    } else {
-      if(step + 1 <= getLastStep()) {
-        new_step = step + 1;
-        new_sub_step = 1;
-      } else {
-        new_step = getLastStep();
-        new_sub_step = getLastSubStep(new_step);
-      }
-    }
-
-    var view = $('.js-tool-view[data-step="' + new_step + '"][data-sub-step="' + new_sub_step + '"]' );
-    return view;
+    transitionOut($('.js-tool-view.is-active'));
+    transitionIn($('.js-tool-step').eq(step).find('.js-tool-view').eq(sub_step));
   }
 
   function goPrevStep() {
-    active_view = $(getPrevStep(active_step, active_sub_step));
+    var data = active_view.split('.');
+    var step = data[0];
+    var sub_step = data[1];
+
+    if($('.js-tool-step').eq(step).find('.js-tool-view').eq(+sub_step - +1).length && (+sub_step - +1 !== -1)) {
+      sub_step = +sub_step - +1;
+      console.log('First');
+    } else if($('.js-tool-step').eq(+step - +1).length && (+step - +1 !== -1)) {
+      step = +step - +1;
+      sub_step = $('.js-tool-step').eq(step).find('.js-tool-view').last().index();
+      console.log('Second');
+    } else {
+      return false;
+    }
+
+    active_view = step + '.' + sub_step;
     updateView();
   }
 
   function goNextStep() {
-    active_view = $(getNextStep(active_step, active_sub_step));
+    var data = active_view.split('.');
+    var step = data[0];
+    var sub_step = data[1];
+
+    if($('.js-tool-step').eq(step).find('.js-tool-view').eq(+sub_step + +1).length) {
+      sub_step = +sub_step + +1;
+      console.log('First ' + sub_step);
+    } else if($('.js-tool-step').eq(+step + +1).length) {
+      step = +step + +1;
+      sub_step = 0;
+      console.log('Second ' + step);
+    } else {
+      console.log('Third ' + step + '.' + sub_step);
+      return false;
+    }
+
+    active_view = step + '.' + sub_step;
     updateView();
   }
 
+
   function addCardBreadcrumbs() {
     $(views).each(function() {
-      var step = $(this).data('step');
-      var sub_step = $(this).data('sub-step');
-      var max_sub_steps = getLastSubStep(step);
-      var title = $('.js-tool-steps').find('[data-step="'+step+'"]').children('.js-tool-step-title').html();
+      var step = $(this).closest('.js-tool-step').index();
+      var sub_step = $(this).index();
+      var max_sub_steps = $(this).closest('.js-tool-step').find('.js-tool-view').last().index();
+
+      var title = $(this).closest('.js-tool-step').attr('data-step-name');
       var text = title + " — " + sub_step + " of " + max_sub_steps;
-      $(this).find('.js-breadcrumb').html(text);
+
+      if($(this).find('.js-breadcrumb').length) {
+        $(this).find('.js-breadcrumb').html(text);
+      }
     });
   }
 
-
-// This function iterates through every step and sub step,
-// and creates a grid with mini-cards linked to their corresponding
-// modals.
-
   function createGrid() {
+
     $(views).each(function() {
-      var step = $(this).data('step');
-      var sub_step = $(this).data('sub-step');
+      var step = $(this).closest('.js-tool-step').index();
+      var sub_step = $(this).index();
+
       var mini = $(this).find('.js-tool-view-mini .js-tool-card');
       var modal = $(this).find('.js-tool-view-contents .js-tool-modal');
 
@@ -201,35 +150,87 @@
     var template = $('.js-tool-grid-block');
     var item_template = $('.js-tool-grid-item');
 
-    // Iterate through every step except the last
-    for (i = 1; i < getLastStep(); i++) {
-      var step = i;
-      var title = $('.js-tool-steps').find('[data-step="'+step+'"]').children('.js-tool-step-title').html();
-      // Set block title
-      var new_block = template.clone().removeClass('js-template');
-      new_block.find('.js-tool-grid-block-title').html(title);
+    $('.js-tool-step').each(function() {
+      var step = $(this).index();
+      var title = $(this).attr('data-step-name');
+      var new_block = "";
 
-      // Set theme based on first sub step
-      var theme = $('.js-tool-view[data-step="'+step+'"]').first().data('theme');
-      new_block.addClass(theme);
+      if($(this).attr('data-grid-include') !== 'false') {
+        var new_block = template.clone().removeClass('js-tool-grid-template').attr('data-linked-step', step);
+        new_block.find('.js-tool-grid-block-title').html(title);
 
-      // Iterate through every sub-step
-      for(x = 0; x < getLastSubStep(step); x++) {
-        var new_item = item_template.clone().removeClass('js-template');
-        var sub_step = x + 1;
-        var sub_step_mini = $('.js-tool-view-mini .js-tool-card[data-linked-step="' + step + '"][data-linked-sub-step="' + sub_step + '"]');
-        sub_step_mini.appendTo(new_item);
-        new_item.appendTo(new_block.find('.js-tool-grid-item-container'));
+        var theme = $(this).attr('data-theme');
+        new_block.addClass(theme);
+
+        $(this).find('.js-tool-view').each(function() {
+          var new_item = "";
+
+          if($(this).attr('data-grid-include') !== 'false') {
+            new_item = item_template.clone().removeClass('js-tool-grid-template');
+            var sub_step_mini = $(this).find('.js-tool-view-mini .js-tool-card');
+            sub_step_mini.appendTo(new_item);
+            new_item.appendTo(new_block.find('.js-tool-grid-item-container'));
+          }
+
+        });
+
+        new_block.appendTo('.js-tool-grid');
       }
-      new_block.appendTo('.js-tool-grid');
-    }
 
-    $('.js-template').remove();
+    });
+
+    $('.js-tool-grid-template').remove();
+
   }
 
 
+  function createNav() {
 
+    var item_template = $('.js-tool-nav-item.js-tool-nav-template');
 
+    $('.js-tool-step').each(function() {
+      if($(this).attr('data-nav-include') !== "false") {
+        var name = $(this).attr('data-step-name');
+        var new_item = item_template.clone().removeClass('js-tool-nav-template').attr('data-linked-step', $(this).index());
+
+        new_item.find('.js-tool-nav-title').html(name);
+        new_item.appendTo('.js-tool-nav-list');
+      }
+    });
+
+    $('.js-tool-nav-template').remove();
+  }
+
+  function updateProgress() {
+    var data = active_view.split('.');
+    var step = data[0];
+    var sub_step = data[1];
+    var nav_item = $('.js-tool-nav-item[data-linked-step="' + step + '"]');
+
+    // If there's an actual nav item linked to this step
+    if(nav_item.length) {
+      $('.js-tool-nav-item').not(nav_item).removeClass('is-active');
+      nav_item.addClass('is-active');
+      var first_offset = $('.js-tool-nav-item').eq(0).find('.js-tool-nav-point').offset().left;
+      var active_offset = nav_item.find('.js-tool-nav-point').offset().left;
+
+      var new_width = active_offset - first_offset;
+
+      $('.js-tool-progress').velocity({ width: new_width });
+    }
+  }
+
+  function updateGrid() {
+    var printable_length = print_array.length;
+    $('.js-tool-grid .js-tool-card').addClass('is-disabled');
+
+    for (i = 0; i < printable_length; i++) {
+      var data = print_array[i].split('.');
+      var step = data[0];
+      var sub_step = data[1];
+      $('.js-tool-grid .js-tool-card[data-linked-step="' + step + '"][data-linked-sub-step="' + sub_step + '"]').removeClass('is-disabled');
+    }
+  }
 
 // Mother function. Initializes everything else
   window.onload = function() {
@@ -241,17 +242,39 @@
 
     container = $('.js-tool');
     main = $('.js-tool-main');
+    steps = $('.js-tool-step');
     views = $('.js-tool-view');
-    active_view = $('.js-tool-view.is-active');
-    active_step = active_view.data('step');
-    active_sub_step = active_view.data('sub-step');
-    active_theme = active_view.data('theme');
+
+    // Set the active view
+    active_view = "0.0";
+
+    // Create the initial grid of cards
+    createGrid();
+
+    // Create navigation
+    createNav();
+
+    // Add breadcrumbs (Step x of x) to cards
+
+    addCardBreadcrumbs();
+
+    // Initial state for velocity
+    transitionOut(views.not(active_view));
+
+    // Update our view to reflect the initial state
+    updateView();
+
+    // Remove the loading screen
+    container.addClass('is-loaded');
+  }
+
+    /*
+
+    assignStepPercentages();
 
     addCardBreadcrumbs();
 
     createGrid();
-
-    container.addClass('is-loaded');
 
     transitionOut(views.not(active_view));
     updateView();
@@ -262,6 +285,8 @@
         goPrevStep();
       }
     });
+    */
+
 
     $('.js-tool-next').click(function() {
       goNextStep();
@@ -272,28 +297,35 @@
     });
 
     $('.js-tool-no').click(function() {
-      var number = active_step + '.' + active_sub_step;
-      addToPrintable(number);
+      addToPrintable(active_view);
+      updateGrid();
       goNextStep();
     });
 
     $('.js-tool-yes').click(function() {
-      var number = active_step + '.' + active_sub_step;
-      removeFromPrintable(number);
+      removeFromPrintable(active_view);
+      updateGrid();
       goNextStep();
     });
 
     $('.js-tool-more').click(function() {
       var parent_view = $(this).closest('[data-linked-step]');
-      var parent_step = parent_view.data('linked-step');
-      var parent_sub_step = parent_view.data('linked-sub-step');
+      var parent_sub_step = $(this).closest('[data-linked-sub-step]');
       var modal = $('.js-tool-modal[data-linked-step="' + parent_step + '"][data-linked-sub-step="' + parent_sub_step + '"]');
-      modal.addClass('is-active');
+      modal.addClass('is-active').addClass('js-tool-printable');
       $('html').addClass('is-modal-active');
     })
 
-    $('.js-modal-close').click(function() {
+    $('.js-tool-modal-close').click(function() {
       $('html').removeClass('is-modal-active');
-      $('.js-tool-modal').removeClass('is-active');
+      $('.js-tool-modal').removeClass('is-active').removeClass('js-tool-printable');
     });
-  }
+
+    $('.js-tool-print').click(function() {
+      window.print();
+    });
+
+    $(window).on('resize', function() {
+      updateMainHeight();
+      updateProgress();
+    });
