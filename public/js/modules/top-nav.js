f2f55a9d96e16c2626aa08a078067bab4692c89a
Mariano Miguel <mariano@mariano.io>
2016-12-20 20:00:24 -0300

    Open/Closed Button Text

--- public/js/modules/top-nav.js
+++ public/js/modules/top-nav.js
@@ -4,10 +4,12 @@ html = $('html');
 
 toggle.click(function() {
   if($(this).attr('aria-expanded') == 'false') {
+    $(this).html('Close');
     $(this).attr('aria-expanded', 'true');
     menu.attr('aria-hidden', 'false');
     html.addClass('is-scroll-locked');
   } else {
+    $(this).html('Menu');
     $(this).attr('aria-expanded', 'false');
     menu.attr('aria-hidden', 'true');
     html.removeClass('is-scroll-locked');
