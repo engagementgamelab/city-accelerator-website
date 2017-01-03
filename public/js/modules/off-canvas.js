eca0d6a3d9b1daca699f1ba556ba8f97d11438b4
Mariano Miguel <mariano@mariano.io>
2017-01-03 13:02:55 -0300

    Off-canvas changes

--- public/js/modules/off-canvas.js
+++ public/js/modules/off-canvas.js
@@ -7,10 +7,12 @@ toggle = $('.js-off-canvas-toggle');
 toggle.click(function() {
   if((element).attr('aria-hidden') == 'true') {
     element.attr('aria-hidden', 'false');
+    toggle.attr('aria-pressed', 'true');
     $(html).addClass('is-scroll-locked');
     toggle.html(toggle.attr('data-expanded'));
   } else {
     element.attr('aria-hidden', 'true');
+    toggle.attr('aria-pressed', 'false');
     $(html).removeClass('is-scroll-locked');
     toggle.html(toggle.attr('data-collapsed'));
   }
@@ -19,6 +21,7 @@ toggle.click(function() {
 $(document).mouseup(function(e) {
   if(!element.is(e.target) && element.has(e.target).length === 0) {
     element.attr('aria-hidden', 'true');
+    toggle.attr('aria-pressed', 'false');
     $(html).removeClass('is-scroll-locked');
     toggle.html(toggle.attr('data-collapsed'));
   }
